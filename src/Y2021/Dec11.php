99834b9490c67ce8e578bca6645d1837b058cd25
Christer Edvartsen <cogo@starzinger.net>
2021-12-11 17:05:58 +0100

    Solution for Dec 11, both parts

---
+++ src/Y2021/Dec11.php
@@ -0,0 +1,109 @@
+<?php declare(strict_types=1);
+namespace AoC\Y2021;
+
+use AoC\Solver;
+use RuntimeException;
+
+class Dec11 implements Solver
+{
+    private const NEIGHBOURS = [
+        [-1, -1], // above left
+        [-1, 0],  // above
+        [-1, 1],  // above right
+        [0, -1],  // left
+        [0, 1],   // right
+        [1, -1],  // below left
+        [1, 0],   // below
+        [1, 1],   // below right
+    ];
+
+    private function parseInput(string $input): array
+    {
+        return array_map(
+            fn (string $line): array => array_map('intval', str_split(trim($line))),
+            explode(PHP_EOL, trim($input)),
+        );
+    }
+
+    public function flash(array &$map, array $willFlash): int
+    {
+        $hasFlashed = [];
+
+        while ([$y, $x] = array_shift($willFlash)) {
+            if (isset($hasFlashed[$y . '.' . $x])) {
+                continue;
+            }
+
+            $hasFlashed[$y . '.' . $x] = [$y, $x];
+
+            foreach (self::NEIGHBOURS as [$adjustY, $adjustX]) {
+                $nY = $y + $adjustY;
+                $nX = $x + $adjustX;
+
+                if (isset($map[$nY][$nX])) {
+                    $map[$nY][$nX] += 1;
+
+                    if ($map[$nY][$nX] > 9 && !isset($hasFlashed[$nY . '.' . $nX])) {
+                        $willFlash[] = [$nY, $nX];
+                    }
+                }
+            }
+        }
+
+        foreach ($hasFlashed as [$y, $x]) {
+            $map[$y][$x] = 0;
+        }
+
+        return count($hasFlashed);
+    }
+
+    public function solvePart1(string $input): int
+    {
+        $map = $this->parseInput($input);
+        $sum = 0;
+
+        for ($step = 0; $step < 100; $step++) {
+            $willFlash = [];
+
+            for ($y = 0; $y < 10; $y++) {
+                for ($x = 0; $x < 10; $x++) {
+                    $map[$y][$x] += 1;
+
+                    if ($map[$y][$x] > 9) {
+                        $willFlash[] = [$y, $x];
+                    }
+                }
+            }
+
+            $sum += $this->flash($map, $willFlash);
+        }
+
+        return $sum;
+    }
+
+    public function solvePart2(string $input): int
+    {
+        $map = $this->parseInput($input);
+        $step = 0;
+
+        while (++$step) {
+            $willFlash = [];
+
+            for ($y = 0; $y < 10; $y++) {
+                for ($x = 0; $x < 10; $x++) {
+                    $map[$y][$x] += 1;
+
+                    if ($map[$y][$x] > 9) {
+                        $willFlash[] = [$y, $x];
+                    }
+                }
+            }
+
+            if (100 === $this->flash($map, $willFlash)) {
+                return $step;
+            }
+        }
+
+        throw new RuntimeException('No solution :(');
+    }
+}
---
+++ tests/Y2021/Dec11Test.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+namespace AoC\Y2021;
+
+use PHPUnit\Framework\TestCase;
+use function AoC\getInputFile;
+
+/**
+ * @coversDefaultClass AoC\Y2021\Dec11
+ */
+class Dec11Test extends TestCase
+{
+    private string $testInput = <<<TESTINPUT
+        5483143223
+        2745854711
+        5264556173
+        6141336146
+        6357385478
+        4167524645
+        2176841721
+        6882881134
+        4846848554
+        5283751526
+    TESTINPUT;
+
+    private Dec11 $solver;
+
+    protected function setUp(): void
+    {
+        $this->solver = new Dec11();
+    }
+
+    /**
+     * @covers ::solvePart1
+     */
+    public function testSolvePart1(): void
+    {
+        $this->assertSame(
+            1656,
+            $this->solver->solvePart1($this->testInput),
+        );
+
+        $this->assertSame(
+            1665,
+            $this->solver->solvePart1(getInputFile(11, 2021)),
+        );
+    }
+
+    /**
+     * @covers ::solvePart2
+     */
+    public function testSolvePart2(): void
+    {
+        $this->assertSame(
+            195,
+            $this->solver->solvePart2($this->testInput),
+        );
+
+        $this->assertSame(
+            235,
+            $this->solver->solvePart2(getInputFile(11, 2021)),
+        );
+    }
+}
